6847b9e888f423dab659c8f90b3ab45bd7129710
Minh Vuong <vuongxuongminh@gmail.com>
2022-08-30 23:52:34 +0700

    docs: add completed example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,15 +1,53 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:go_router/src/state.dart';
+import 'package:go_router/go_router.dart';
 import 'package:go_router_plus/go_router_plus.dart';
 
+// ignore_for_file: public_member_api_docs
+
+/// An example app using Go Router plus.
 void main() {
-  final router = createGoRouter(screens: [LoginScreen(), HomeScreen()]);
+  final authService = AuthService();
+  final router = createGoRouter(
+    screens: [
+      LoginScreen(authService),
+      HomeScreen(authService),
+    ],
+    redirectors: [
+      ScreenRedirector(),
+      AuthRedirector(
+        state: authService,
+        guestRedirectPath: '/login',
+        userRedirectPath: '/home',
+      )
+    ],
+    refreshNotifiers: [
+      /// refresh router when user logged in or logged out
+      authService,
+    ],
+  );
+
+  runApp(
+    MaterialApp.router(
+      routeInformationParser: router.routeInformationParser,
+      routerDelegate: router.routerDelegate,
+      routeInformationProvider: router.routeInformationProvider,
+    ),
+  );
 }
 
 class LoginScreen extends Screen implements InitialScreen, GuestScreen {
+  LoginScreen(this.authService);
+
+  final AuthService authService;
+
   @override
-  builder(BuildContext context, GoRouterState state) => Scaffold(body: Text('Login screen'));
+  Widget builder(BuildContext context, GoRouterState state) => Scaffold(
+        body: const Text('Login screen'),
+        floatingActionButton: FloatingActionButton(
+          onPressed: authService.login,
+          child: const Icon(Icons.login),
+        ),
+      );
 
   @override
   String get routeName => 'login';
@@ -19,12 +57,45 @@ class LoginScreen extends Screen implements InitialScreen, GuestScreen {
 }
 
 class HomeScreen extends Screen implements UserScreen {
+  HomeScreen(this.authService);
+
+  final AuthService authService;
+
   @override
-  builder(BuildContext context, GoRouterState state) => Scaffold(body: Text('Home screen'));
+  Widget builder(BuildContext context, GoRouterState state) => Scaffold(
+        body: const Text('Home screen'),
+        floatingActionButton: FloatingActionButton(
+          onPressed: authService.logout,
+          child: const Icon(Icons.logout),
+        ),
+      );
 
   @override
   String get routeName => 'home';
 
   @override
   String get routePath => '/home';
+}
+
+/// Authentication service providing current user state (logged in/out),
+/// and notify router to trigger redirection logic when state change.
+class AuthService with ChangeNotifier implements LoggedInState {
+  bool _loggedIn = false;
+
+  @override
+  bool get loggedIn => _loggedIn;
+
+  set loggedIn(bool value) {
+    _loggedIn = value;
+  }
+
+  void login() {
+    loggedIn = true;
+    notifyListeners();
+  }
+
+  void logout() {
+    loggedIn = false;
+    notifyListeners();
+  }
 }
--- example/test/router_test.dart
+++ example/test/router_test.dart
@@ -1,1 +1,48 @@
-void main() {}
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:go_router_plus/go_router_plus.dart';
+import 'package:go_router_plus_example/main.dart';
+
+void main() {
+  testWidgets('test app using Go Router plus', (tester) async {
+    final authService = AuthService();
+    final router = createGoRouter(
+      screens: [
+        LoginScreen(authService),
+        HomeScreen(authService),
+      ],
+      redirectors: [
+        ScreenRedirector(),
+        AuthRedirector(
+          state: authService,
+          guestRedirectPath: '/login',
+          userRedirectPath: '/home',
+        )
+      ],
+      refreshNotifiers: [
+        /// refresh router when user logged in or logged out
+        authService,
+      ],
+    );
+
+    await tester.pumpWidget(
+      MaterialApp.router(
+        routeInformationParser: router.routeInformationParser,
+        routerDelegate: router.routerDelegate,
+        routeInformationProvider: router.routeInformationProvider,
+      ),
+    );
+
+    expect(find.text('Login screen'), findsOneWidget);
+
+    await tester.tap(find.byIcon(Icons.login));
+    await tester.pumpAndSettle();
+
+    expect(find.text('Home screen'), findsOneWidget);
+
+    await tester.tap(find.byIcon(Icons.logout));
+    await tester.pumpAndSettle();
+
+    expect(find.text('Login screen'), findsOneWidget);
+  });
+}
